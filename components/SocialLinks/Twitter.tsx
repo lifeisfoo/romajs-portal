6e4f7b53b62885b62b7df2f93a0351a01700f0e1
DonKy <donky@MacB.local>
2022-04-05 19:38:15 +0200

    Fix social links

--- components/SocialLinks/Twitter.tsx
+++ components/SocialLinks/Twitter.tsx
@@ -1,4 +1,4 @@
-import { ImTwitter as TwitterIcon } from "react-icons/Im";
+import { FaTwitter as TwitterIcon } from "react-icons/fa";
 import SocialSquare from "./SocialSquare";
 const href = "https://twitter.com/roma_j";
 export default function Twitter(): JSX.Element {
